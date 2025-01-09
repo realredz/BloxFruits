a4545ec6ac924a63d76fee06a2f1a858fedc082b
realredz <pauloguiluerme2009@gmail.com>
2025-01-09 10:50:49 -0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,1 +1,11 @@
+local Settings = select(1, ...) or {
+  Translator = true,
+  JoinTeam = "Pirates",
+  Kaitun = false
+}
 
+local Owner = "realredz"
+local Repository = `https://raw.githubusercontent.com/{ Owner }/BloxFruits/`
+local SourceName = if Settings.Kaitun then "Kaitun" else "Main"
+
+loadstring(game:HttpGet(`{ Repository }refs/heads/main/Scripts/{ SourceName }.lua`))(Settings)
