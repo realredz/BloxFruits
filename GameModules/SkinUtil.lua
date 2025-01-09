ee37a9baaeaa09fc422953de48047e88237a84ac
realredz <pauloguiluerme2009@gmail.com>
2025-01-09 10:38:57 -0300

    Create SkinUtil.lua

---
+++ GameModules/SkinUtil.lua
@@ -0,0 +1,57 @@
+return {
+  ["Bright Yellow"] = {
+    EtcItems = {
+      { Name = "Yellow Star Berry", Amount = 1 }
+    }
+  },
+  ["Yellow Sunshine"] = {
+    EtcItems = {
+      { Name = "Yellow Star Berry", Amount = 1 },
+      { Name = "White Cloud Berry", Amount = 1 }
+    }
+  },
+  ["Snow White"] = {
+    EtcItems = {
+      { Name = "White Cloud Berry", Amount = 10 }
+    }
+  },
+  ["Orange Soda"] = {
+    EtcItems = {
+      { Name = "Orange Berry", Amount = 1 }
+    }
+  },
+  ["Fiery Rose"] = {
+    EtcItems = {
+      { Name = "Pink Pig Berry", Amount = 2 },
+      { Name = "Red Cherry Berry", Amount = 2 }
+    }
+  },
+  Dragon = {
+    EtcItems = {
+      { Name = "Red Cherry Berry", Amount = 6 },
+      { Name = "Pink Pig Berry", Amount = 8 },
+      { Name = "Orange Berry", Amount = 3 }
+    }
+  },
+  ["Green Lizard"] = {
+    EtcItems = {
+      { Name = "Green Toad Berry", Amount = 1 },
+      { Name = "White Cloud Berry", Amount = 1 }
+    }
+  },
+  ["Plump Purple"] = {
+    EtcItems = {
+      { Name = "Purple Jelly Berry", Amount = 3 }
+    }
+  },
+  ["Winter Sky"] = {
+    EtcItems = {
+      { Name = "Pink Pig Berry", Amount = 15 }
+    }
+  },
+  ["Pure Red"] = {
+    EtcItems = {
+      { Name = "Red Cherry Berry", Amount = 15 }
+    }
+  }
+}
